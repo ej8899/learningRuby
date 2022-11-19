9bc7e9ce37b4d26742a46ea5b903501e3996d0a2
ej8899 <ej8899@gmail.com>
2022-11-18 17:16:50 -0700

    chore: add ex3 from learing ruby the hard way

--- ex1.rb
+++ ex1.rb
@@ -1,3 +1,10 @@
+#
+# learn ruby the hard way
+#
+# https://flex-web.compass.lighthouselabs.ca/workbooks/flex-m09w22/activities/1037?journey_step=57
+# https://learnrubythehardway.org/book/ex1.html
+#
+
 puts "Hello World!"
 puts "Hello Again"
 puts "I like typing this."
